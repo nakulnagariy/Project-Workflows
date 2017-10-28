693701294e5712acf4f99aa4b9302d7552ad6586
nakulnagariy <nagariya23nakul@gmail.com>
2017-10-28 18:15:13 +0530

    CoffeeFiles added

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,12 @@
 var gulp = require('gulp'),
-	gutil = require('gulp-util');
+	gutil = require('gulp-util'),
+	coffee = require('gulp-coffee');
 
-gulp.task('log', function(){
-	gutil.log('Workflows are awesome');
+var coffeeSourses = ['components/coffee/tagline.coffee'];
+
+gulp.task('coffee', function() {
+	gulp.src(coffeeSourses)
+		.pipe(coffee({ bare: true })
+			.on('error', gutil.log))
+		.pipe(gulp.dest('components/scripts'))
 });
