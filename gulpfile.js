9690e38decf8e32f4cee99c881ce6254dbbdd51a
nakulnagariy <nagariya23nakul@gmail.com>
2017-10-28 18:23:26 +0530

    Concatenated javascript files added

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,16 @@
 var gulp = require('gulp'),
 	gutil = require('gulp-util'),
-	coffee = require('gulp-coffee');
+	coffee = require('gulp-coffee'),
+	concat = require('gulp-concat');
 
 var coffeeSourses = ['components/coffee/tagline.coffee'];
+
+var jsSourses = [
+	'components/scripts/pixgrid.js',
+	'components/scripts/rclick.js',
+	'components/scripts/tagline.js',
+	'components/scripts/template.js'
+	];
 
 gulp.task('coffee', function() {
 	gulp.src(coffeeSourses)
@@ -10,3 +18,9 @@ gulp.task('coffee', function() {
 			.on('error', gutil.log))
 		.pipe(gulp.dest('components/scripts'))
 });
+
+gulp.task('js', function() {
+	gulp.src(jsSourses)
+		.pipe(concat('scrit.js'))
+		.pipe(gulp.dest('bulids/development/js'))
+})
