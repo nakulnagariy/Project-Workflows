fd089051cea03453388586c2367785706130e709
nakulnagariy <nagariya23nakul@gmail.com>
2017-10-29 18:18:14 +0530

    Live reloading done

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gulp = require('gulp'),
 	coffee = require('gulp-coffee'),
 	browserify = require('gulp-browserify'),
 	compass = require('gulp-compass'),
+	connect = require('gulp-connect'),
 	watch = require('gulp-watch'),
 	concat = require('gulp-concat');
 
@@ -29,6 +30,7 @@ gulp.task('js', function() {
 		.pipe(concat('script.js'))
 		.pipe(browserify())
 		.pipe(gulp.dest('builds/development/js'))
+		.pipe(connect.reload())
 });
 
 gulp.task('compass', function() {
@@ -40,6 +42,7 @@ gulp.task('compass', function() {
 		}))
 		.on('error', gutil.log)
 		.pipe(gulp.dest('builds/development/css'))
+		.pipe(connect.reload())
 });
 
 gulp.task('watch', function() {
@@ -48,4 +51,11 @@ gulp.task('watch', function() {
 	gulp.watch('components/sass/*', ['compass']);
 });
 
-gulp.task('default', ['coffee', 'js', 'compass']);
+gulp.task('connect', function() {
+	connect.server({
+		root: 'builds/development/',
+		livereload: true
+	});
+});
+
+gulp.task('default', ['coffee', 'js', 'compass', 'connect', 'watch']);
